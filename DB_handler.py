56123062e50cd771e1f51442f694a0d92e2b87b4
unknown <d0abigor@gmail.com>
2025-11-22 22:51:19 +0530

    barebones frontend connected to backend

--- DB_handler.py
+++ DB_handler.py
@@ -2,21 +2,24 @@ import asyncpg
 import asyncio
 import os
 from dotenv import load_dotenv
+from passlib.hash import bcrypt
 
 
+verbose = True
 load_dotenv()
 
 conn_pool = None
 
-async def init_conn_pool():
+async def init_conn_pool_and_cleaner():
     global conn_pool
-    conn_pool = asyncpg.create_pool(
+    conn_pool = await asyncpg.create_pool(
         user=os.getenv("PSQUSER"),
         password=os.getenv("PSQPASS"),
         database="mlscproject1",
         host = "localhost",
         port = 9112
     )
+    asyncio.create_task(session_cleaner())
 
 async def get_admin_landing():
     async with conn_pool.acquire() as connection:
@@ -24,15 +27,17 @@ async def get_admin_landing():
         return rows
 
 
-async def get_session_id(username,password_hash):
+async def get_session_id(username,password):
     async with conn_pool.acquire() as connection:
-        rows = await connection.fetchrow("SELECT id,username,password_hash FROM users WHERE username = $1 AND password_hash = $2;", username, password_hash)
-        if rows:
-            session_id = await connection.fetchrow("INSERT INTO sessions (user_id, expires_at) VALUES ($1, NOW() + INTERVAL '1 day') RETURNING session_id;",rows['id'] )
-            return True,session_id["session_id"]
+        passhash = await connection.fetchrow("SELECT id,username,password_hash FROM users WHERE username = $1;", username)
+
+        if passhash:
+            if bcrypt.verify(password, passhash["password_hash"]):
+                ## add check for checking exsiting active tokens
+                session_id = await connection.fetchrow("INSERT INTO sessions (user_id, expires_at) VALUES ($1, NOW() + INTERVAL '1 day') RETURNING session_id;",passhash['id'] )
+                return True,session_id["session_id"]
         else:
             return False,"invalid credentials"
-
 
 async def validate_session_id(sessionid):
     async with conn_pool.acquire() as connection:
@@ -49,25 +54,34 @@ async def session_cleaner():
         await asyncio.sleep(300)
     
 
-async def register_user(email,username,password_hash,access_level):
+async def register_user(email,username,password,access_level):
     async with conn_pool.acquire() as connection:
         conflict = await connection.fetchrow("SELECT username FROM users WHERE username = $1", username)
+        password_hashed = await hashpass(password)
         if conflict:
             return False,"Username already in DB, Try logging in"
         else:
-            await connection.execute("INSERT INTO users (email,username,password_hash,access) VALUES ($1,$2,$3,$4');", email, username, password_hash, access_level)
+            await connection.execute("INSERT INTO users (email,username,password_hash,access) VALUES ($1,$2,$3,$4);", email, username, password_hashed, access_level)
             return True, "registered new user"
 
 
 async def get_access_level(session_id):
+    if verbose:
+        print("getting access level for/home endpoint")
     async with conn_pool.acquire() as connection:
-        row = await connection.fetchrow("""
-    SELECT user.access
-    FROM sessions session
-    JOIN users user ON session.user_id = user.id
-    WHERE session.session_id = $1
-""", session_id)
+        row = await connection.fetchrow("SELECT u.access FROM sessions s JOIN users u ON s.user_id = u.id WHERE s.session_id = $1;", session_id)
         if row:
             return True, row["access"]
         else:
             return False, "could not get access level"
+        
+
+async def delete_session_id(session_id):
+    async with conn_pool.acquire() as connection:
+        await connection.execute("DELETE FROM sessions WHERE session_id = $1", session_id)
+
+async def hashpass(password):
+    hashed = await asyncio.to_thread(bcrypt.using(rounds=12).hash, str(password))
+    return hashed
+
+
--- main.py
+++ main.py
@@ -3,20 +3,18 @@ from fastapi.responses import RedirectResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 import DB_handler
-import asyncio
-from passlib.hash import bcrypt
 from contextlib import asynccontextmanager
 
-
+verbose = False
 
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await DB_handler.init_conn_pool()                       ## postgress module and its helpers init
-    asyncio.create_task(DB_handler.session_cleaner())
+    await DB_handler.init_conn_pool_and_cleaner()                       ## postgress module and its helpers init
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 pages = Jinja2Templates(directory="frontend")
 
 
@@ -30,21 +28,31 @@ app.add_middleware(
 
 @app.middleware("http")
 async def auth_middleware(request: Request, call_next):
-
+    if verbose:
+        print("executing middleware") 
     public_endpoint = ["/", "/userlogin", "/adminlogin", "/register", "/invalid"]
+
     if request.url.path in public_endpoint:
         return await call_next(request)
     
     id = request.cookies.get("session_id")
+    if verbose:
+        print("cookie detected by middleware:", id)
     if not id:
         return RedirectResponse("/invalid")
     
     status = await DB_handler.validate_session_id(id)
 
     if status:
+        if verbose:
+            print("middleware cookie validation pass!")
         response = await call_next(request)
+        if verbose:
+            print(response)
         return response
     else:
+        if verbose:
+            print("middleware cookie validation fail")
         return RedirectResponse("/invalid")
     
 
@@ -72,36 +80,47 @@ async def register_get(request: Request):
 async def invalid(request: Request):
     return pages.TemplateResponse("invalid.html", {"request": request})
 
+@app.get("/logout")
+async def logout(request: Request):
+    session_id = request.cookies.get("session_id")
+    DB_handler.delete_session_id(session_id)
 
-@app.post("/userlogin")                         # request = {"username" : <username>, "password": <password>}
+    response = RedirectResponse("/", status_code=303)
+    response.delete_cookie("session_id")
+    return response
+
+
+@app.post("/userlogin")                         
 async def userlogin_post(request: Request):
     data = await request.json()
-
-    status, session_id = await DB_handler.get_session_id(data.get("username"), hashpass(data.get("password")))["session_id"]
+    status, session_id = await DB_handler.get_session_id(data.get("username"), data.get("password"))
     if status!=True:
         return RedirectResponse("/invalid")
     
     redirect = RedirectResponse(url="/home", status_code=303)
     redirect.set_cookie(key="session_id", value=str(session_id), httponly=True)
+    redirect.set_cookie(key="origin", value="userlogin")
     return redirect
 
-@app.post("/adminlogin")                        # request = {"username" : <username>, "password": <password>}
+@app.post("/adminlogin")
 async def adminlogin_post(request: Request):
     data = await request.json()
 
-    status, session_id = await DB_handler.get_session_id(data.get("username"), hashpass(data.get("password")))["session_id"]
+    status, session_id = await DB_handler.get_session_id(data.get("username"), data.get("password"))
+    if verbose:
+        print(status, session_id)
     if status!=True:
         return RedirectResponse("/invalid")
     
     redirect = RedirectResponse(url="/home", status_code=303)
     redirect.set_cookie(key="session_id", value=str(session_id), httponly=True)
+    redirect.set_cookie(key="origin", value="adminlogin")
     return redirect
 
-@app.post("/register")                          # request = {"emailid": <email>, "username" : <username>, "password": <password>, "access_level": <access_level>}
+@app.post("/register")                   
 async def register_post(request: Request):
-    data = request.json()
-    
-    status, response = await DB_handler.register_user(data.get("emailid"), data.get("username"), hashpass(data.get("password")), data.get("type"))
+    data = await request.json()
+    status, response = await DB_handler.register_user(data.get("emailid"), data.get("username"), data.get("password"), data.get("type"))
 
     return JSONResponse({"message": response})
 
@@ -109,33 +128,44 @@ async def register_post(request: Request):
 
 @app.get("/home")
 async def gethome(request: Request):
-   
+   if verbose:
+       print("at home")                         #####################################
+
    id = request.cookies.get("session_id")
+   if verbose:
+       print("cookie detected inside home", id)
    status, role = await DB_handler.get_access_level(id)
+   if verbose:
+       print("status and role:",status,role)
 
    if status:
-        if role == "admin":
-            package = adminhome()
+        if verbose:
+            print("positive status")
+        if role == "admin" and request.cookies.get("origin") == "adminlogin":
+            if verbose:
+                print("detected login as admin")
+            package = await adminhome(request)
+            if verbose:
+                print("package", package)
             return package
-        elif role == "user":
-            package = userhome()
+        elif role == "user" and request.cookies.get("origin") == "userlogin":
+            package = await userhome(request)
             return package
    else:
        return {"error": role}
 
 async def adminhome(request: Request):
     rows = await DB_handler.get_admin_landing()
-    users = [dict(row) for row in rows]         
+    users = [dict(row) for row in rows] 
+    if verbose:
+        print("users", users)        
     return pages.TemplateResponse(
         "adminhome.html",
         {"request": request, "users": users}
     )
 
-def userhome():
+async def userhome(request):
     return pages.TemplateResponse(
-        "userhome.html"
+        "userhome.html",
+        {"request": request}
     )
-
-def hashpass(password):
-    hashed = bcrypt.using(rounds=12).hash(password)
-    return hashed
